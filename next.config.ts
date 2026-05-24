9df019530f46becd7cb4050bcfa869959635933e
Ahmad Rifai <ahmmd.riffai@gmail.com>
2026-05-24 15:18:25 +0700

    fix: image domain

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
       "images.unsplash.com",
       "localhost",
       "lh3.googleusercontent.com",
+      "diengs.id",
     ],
     dangerouslyAllowLocalIP: true, // 🔥 ini kunci
   },
